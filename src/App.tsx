a4f2a39206026b1b5b91e806510f071df12a4a43
danny <daniel.mcgrane@zebedee.io>
2022-02-19 21:35:21 +0000

    websockets wokring

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,13 @@ import { Auth } from "./components/Auth";
 import { Upload } from "./components/Upload";
 import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
-import QRCode from "react-qr-code";
 import axios from "axios";
+import { PaymentFlow } from "./components/PaymentFlow";
 
 const App = () => {
   const [user, setUser] = useState<string>("");
   const [cookies, setCookie, removeCookie] = useCookies(["user"]);
   const [invoice, setInvoice] = useState();
-  const [isPaid, useIsPaid] = useState();
 
   const handleUserSession = (token: string) => {
     setUser(token);
@@ -29,9 +28,10 @@ const App = () => {
   };
 
   const getInvoice = async () => {
-    const data: any = await axios.get(`http://localhost:4008/api/getInvoice`);
-    setInvoice(data.data.lightning_invoice.payreq);
-    console.log({ data });
+    const data: any = await axios.get(
+      `http://localhost:4008/api/generateInvoice`
+    );
+    setInvoice(data.data.invoice.payment_request);
   };
 
   useEffect(() => {
@@ -50,21 +50,14 @@ const App = () => {
       ) : (
         <Upload logOut={logOut} />
       )}
-
       {/* TESTING WEBSOCKETS */}
-
       <button
         style={{ marginTop: 100, marginBottom: 100 }}
         onClick={getInvoice}
       >
         Get invoice
       </button>
-      {invoice && (
-        <div>
-          <QRCode value={invoice} />
-        </div>
-      )}
-      {isPaid && <div style={{ fontSize: 30, color: "green" }}>PAID</div>}
+      {invoice && <PaymentFlow invoice={invoice} setInvoice={setInvoice} />}
     </div>
   );
 };
